3b3912c666a787b773af478e751b88530498be33
cymi-eng <eaiancymi@gmail.com>
2026-07-10 11:08:58 +0300

    create auth context

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HostelDetails from './components/HostelDetails'
 import HomePage from './pages/HomePage'
 import Profile from './pages/Profile';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
          
         <Route path="/hostels/:id" element={<HostelDetails/>} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path="/register" element={<Register/>} />
         
       </Routes>
     
