14fb7dca26e253ffc14e37f953d6cef7b7c2f7e7
derrickweru <derrickweru73@gmail.com>
2026-07-10 11:26:48 +0300

    feat: add contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HostelDetails from './components/HostelDetails'
 import HomePage from './pages/HomePage'
 import Profile from './pages/Profile';
-
+import Contact from './pages/Contact';
 function App() {
   return (
     <>
       <Router>
-       <HomePage/>
+       {/* <HomePage/> */}
       <Routes>
          
+        <Route path="/" element={<HomePage/>} />
         <Route path="/hostels/:id" element={<HostelDetails/>} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path="/contact" element={<Contact/>} />
         
       </Routes>
     
