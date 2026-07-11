cbadc9064da5858a7d9e585e203e9c417a4a1c14
derrickweru <derrickweru73@gmail.com>
2026-07-11 21:25:59 +0300

    feat : update new changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+ import React, { useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
