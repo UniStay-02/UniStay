93cf2f69361d1e8bb3f292db5926b6880df9e41b
Jblue254 <japhethkiprono2020@gmail.com>
2026-07-12 20:00:59 +0500

    feat : imports needed by the admin route

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useAuth } from '@/context/AuthContext';
+import { Navigate, useLocation } from 'react-router-dom';
 
 function AdminRoute() {
   return (
-    <div>AdminRoute</div>
+    <>
+
+
+    </>
   )
 }
 
